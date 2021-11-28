9dff04314da7b81a12e06e86e1e955de101966cc
kubesure <edakghar@gmail.com>
2021-11-29 00:00:46 +0400

    stair submission

---
+++ main/stair_mian.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//var h int
+
+	//fmt.Scan(&h)
+	h := 4
+
+	if h < 1 || h > 100 {
+		return
+	}
+
+	str := ""
+	for i := h; i > 0; i-- {
+		str += "#"
+		s := strconv.Itoa(h)
+		fmt.Printf("%"+s+"s\n", str)
+	}
+}
