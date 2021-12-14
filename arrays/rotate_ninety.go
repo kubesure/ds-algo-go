d81a025d4182f7e133fca44ce3a50dc8a33a8a5f
kubesure <edakghar@gmail.com>
2021-12-14 23:01:35 +0400

    fixed by not adding indexs

--- arrays/rotate_ninety.go
+++ arrays/rotate_ninety.go
@@ -13,16 +13,16 @@ func rotate90(cube [][]int) ([][]int, error) {
 
 	printCube(cube)
 
-	for layer := 0; layer < cubeSize/2; layer++ {
-		for j := layer; j < cubeSize-1-layer; j++ {
-			temp := cube[layer][j]
-			cube[layer][j] = cube[cubeSize-1-j][layer] // 7 > 1 // 4 > 2
+	for i := 0; i < cubeSize/2; i++ {
+		for j := i; j < cubeSize-1; j++ {
+			temp := cube[i][j]
+			cube[i][j] = cube[cubeSize-1-j][i] // 7 > 1 // 4 > 2
 			//printCube(cube)
-			cube[cubeSize-1-j][layer] = cube[cubeSize-1][cubeSize-1-j] // 9 > 7 // 8 > 4
+			cube[cubeSize-1-j][i] = cube[cubeSize-1][cubeSize-1-j] // 9 > 7 // 8 > 4
 			//printCube(cube)
-			cube[cubeSize-1][cubeSize-1-j] = cube[layer+j][cubeSize-1] // 3 > 9 // 6 > 8
+			cube[cubeSize-1][cubeSize-1-j] = cube[j-i][cubeSize-1] // 3 > 9 // 6 > 8
 			//printCube(cube)
-			cube[layer+j][cubeSize-1] = temp // 1 > 3 // 8 > 4
+			cube[j-i][cubeSize-1] = temp // 1 > 3 // 8 > 4
 			//printCube(cube)
 		}
 	}
@@ -35,7 +35,7 @@ func printCube(cube [][]int) {
 		for _, column := range row {
 			fmt.Printf("%v ", column)
 		}
-		fmt.Printf("\n")
+		fmt.Printf("\n \n")
 	}
 	fmt.Printf("\n")
 }
