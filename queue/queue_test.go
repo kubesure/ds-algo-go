c7c2e07131bf1448ba8806593dd93bd1e4bf6bd0
kubesure <edakghar@gmail.com>
2022-08-31 23:38:08 +0530

    fixed Dequeue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -12,11 +12,32 @@ func TestQueueCreate(t *testing.T) {
 	q.EnQueue(4)
 	q.EnQueue(5)
 
-	q.DeQueue()
-	q.DeQueue()
-	q.DeQueue()
-	q.DeQueue()
-	q.DeQueue()
+	e := q.DeQueue()
+	if e.value != 1 {
+		t.Errorf("wanted 1 got %v", e.value)
+	}
+
+	e = q.DeQueue()
+	if e.value != 2 {
+		t.Errorf("wanted 2 got %v", e.value)
+	}
+
+	e = q.DeQueue()
+
+	if e.value != 3 {
+		t.Errorf("wanted 3 got %v", e.value)
+	}
+
+	e = q.DeQueue()
+	if e.value != 4 {
+		t.Errorf("wanted 4 got %v", e.value)
+	}
+
+	e = q.DeQueue()
+	if e.value != 5 {
+		t.Errorf("wanted 5 got %v", e.value)
+	}
+
 	val := q.DeQueue()
 
 	if val != nil {
